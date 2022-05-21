ffb0283524ac59f515065548acedc790abe19efe
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-05-21 07:28:59 -0400

    foundation for video page

--- src/components/GryphRouter.tsx
+++ src/components/GryphRouter.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "../pages/homepage";
-import Livestream from "../pages/video";
+import Video from "../pages/video";
 import Login from "../pages/login";
 import Member from "../pages/member";
-import PrimaryVideo from "../pages/livestream";
+import Livestream from "../pages/livestream";
 import Registration from "../pages/registration";
 import Statistics from "../pages/statistics";
 
@@ -12,12 +12,12 @@ export default function GryphRouter() {
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/livestream" element={<Livestream />} />
+        <Route path="/video/id_here" element={<Video />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/statistics" element={<Statistics />} />
         <Route path="/member" element={<Member />} />
-        <Route path="/primary-video" element={<PrimaryVideo />} />
+        <Route path="/Livestream" element={<Livestream />} />
       </Routes>
     </Router>
   );
--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -1,7 +1,93 @@
-import Box from "@mui/material/Box";
+import {
+  Card,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import Box from "@mui/system/Box";
+import MainTemplate from "../template/main-template";
+
+// refactor with video and link to video when time comes
+// video thumbnail will be in a clickable button, yay
+interface TimestampVideoCardProps {
+  thumbnailLink: string;
+  title: string;
+}
+const TimestampVideoCard = ({
+  thumbnailLink,
+  title,
+}: TimestampVideoCardProps) => {
+  console.log(thumbnailLink);
+  return (
+    <ListItem disablePadding>
+      <ListItemButton
+        sx={{
+          height: 200,
+          backgroundColor: "#A3A3A3",
+          "&:hover": {
+            backgroundColor: "#C2C2C2",
+          },
+        }}
+        component="a"
+        href="#simple-list"
+      >
+        <Box
+          sx={{
+            height: "100%",
+          }}
+        />
+      </ListItemButton>
+      <ListItemText sx={{ color: "#A4A4A4" }} primary={title} />
+    </ListItem>
+  );
+};
 
 const Video = () => {
-  return <Box> livestream </Box>;
+  const arrOfTimestamps = [
+    { thumbnailLink: "blah", title: "01:10:30 - 01:17:20" },
+    { thumbnailLink: "blah2", title: "01:02:30 - 01:04:20" },
+  ];
+
+  return (
+    <MainTemplate>
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography variant="h5">Video Title</Typography>
+        </Grid>
+        <Grid container columns={{ xs: 3 }}>
+          <Grid item xs={2}>
+            <Card>
+              <Box
+                sx={{
+                  height: 300,
+                  backgroundColor: "#A3A3A3",
+                }}
+              />
+            </Card>
+            <Box py={2} />
+            <Card> transcript </Card>
+          </Grid>
+          <Grid item xs={1} px={6}>
+            <nav aria-label="timestamps">
+              <Typography variant="h5">Timestamps</Typography>
+              <List>
+                {arrOfTimestamps.map((timestamp, index) => (
+                  <TimestampVideoCard
+                    key={index}
+                    thumbnailLink={timestamp.thumbnailLink}
+                    title={timestamp.title}
+                  />
+                ))}
+              </List>
+            </nav>
+          </Grid>
+        </Grid>
+      </Grid>
+    </MainTemplate>
+  );
 };
 
 export default Video;
