a0ae21765409177aabf1ee26e664ff0bdd681dd0
Duy Nguyen <duynguyenquykhanh@gmail.com>
2022-05-21 11:33:17 -0700

    add signup functionality

--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -10,17 +10,29 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const theme = createTheme();
 
 export default function Registration() {
+  const navigate = useNavigate();
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+
+    axios
+      .post("http://localhost:5000/users", {
+        name: data.get("name"),
+        type: 0,
+        email: data.get("email"),
+        password: data.get("password"),
+      })
+      .then((res) => {
+        navigate("/login");
+        console.log(res);
+      });
   };
 
   return (
@@ -63,6 +75,15 @@ export default function Registration() {
               onSubmit={handleSubmit}
               sx={{ mt: 1 }}
             >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Username"
+                name="name"
+                autoFocus
+              />
               <TextField
                 margin="normal"
                 required
